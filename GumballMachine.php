86627ea98574c81858219a460752cf37a3a3bbee
shivamchandra3 <42465900+shivamchandra3@users.noreply.github.com>
2018-10-14 23:35:45 +0530

    Add files via upload

---
+++ GumballMachine.php
@@ -0,0 +1,18 @@
+class GumballMachine{
+    private $gumballs;
+
+    // Get the amount of gumballs still in the maching
+    public function getGumballs(){
+        return $this->gumballs;
+    }
+
+    //set the amount of gumballs in the machine
+    public function setGumballs($amount){
+        $this->gumballs= $amount;
+    }
+
+    //The user turns the wheel, machine dispenses gumball
+    public function turnWheel(){
+        $this->setGumballs($this->getGumballs() -1);
+    }
+}
